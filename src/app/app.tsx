1401cedfa16416a2d5a3323c3d81b3644cb8e7e8
eduardo-rdguez <eduardoperaltardgz@gmail.com>
2023-05-04 15:03:48 -0600

    Include home and about-me routes to app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,10 +1,16 @@
-import NxWelcome from './nx-welcome';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './home';
+import AboutMe from './about-me';
+import { theme } from './theme';
 
 export function App() {
   return (
-    <ChakraProvider>
-      <NxWelcome title="eduardo-rdguez.github.io" />
+    <ChakraProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
     </ChakraProvider>
   );
 }
